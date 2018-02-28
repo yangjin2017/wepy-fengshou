11f655ce1382eeda936cd3802932d329632ed59b
yangjin2017 <18402901811@qq.com>
2018-02-28 22:40:30 +0800

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,102 @@
+export const data = [
+  {
+    'seq': '1',
+    'name': '莆田西征管所网点',
+    'address': '莆田市城厢区华亭镇山牌村莆田西征管所办公楼',
+    'phone': '0594-2528691',
+    'latitude': '25.399509',
+    'longitude': '118.977344',
+    'preview_image': 'http://ostb6zm4z.bkt.clouddn.com/etc-pt.jpg?imageView2/1/w/120/h/120/format/jpg/q/75|imageslim',
+    'images': [
+      'http://ostb6zm4z.bkt.clouddn.com/etc-pt.jpg'
+    ]
+  },
+  {
+    'seq': '2',
+    'name': '仙游城区征管所网点',
+    'address': '莆田市仙游县鲤南镇下楼村高速公路仙游城区征管所办公楼',
+    'phone': '0594-8170088',
+    'latitude': '25.376232',
+    'longitude': '118.735901',
+    'preview_image': 'http://ostb6zm4z.bkt.clouddn.com/etc-xy.jpg?imageView2/1/w/120/h/120/format/jpg/q/75|imageslim',
+    'images': [
+      'http://ostb6zm4z.bkt.clouddn.com/etc-xy.jpg'
+    ]
+  },
+  {
+    'seq': '3',
+    'name': '涵江江口征管所网点',
+    'address': '莆田市涵江区涵江区江口镇新前村高速公路涵江江口征管所办公楼',
+    'phone': '0594-6263205',
+    'latitude': '25.492494',
+    'longitude': '119.188597',
+    'preview_image': 'http://ostb6zm4z.bkt.clouddn.com/etc-hjk.jpg?imageView2/1/w/120/h/120/format/jpg/q/75|imageslim',
+    'images': [
+      'http://ostb6zm4z.bkt.clouddn.com/etc-hjk.jpg'
+    ]
+  },
+  {
+    'seq': '4',
+    'name': '市行政服务中心网点',
+    'address': '莆田市荔城区荔城中大道1998号3号楼市行政大厅85号窗口',
+    'phone': '0594-2123338',
+    'latitude': '25.451307',
+    'longitude': '119.008712',
+    'preview_image': 'http://ostb6zm4z.bkt.clouddn.com/etc-xz.jpg?imageView2/1/w/120/h/120/format/jpg/q/75|imageslim',
+    'images': [
+      'http://ostb6zm4z.bkt.clouddn.com/etc-xz.jpg'
+    ]
+  },
+  {
+    'seq': '5',
+    'name': '秀屿埭头所网点',
+    'address': '莆田市秀屿区埭头镇武盛村高速公路秀屿埭头收费站12车道',
+    'phone': '0594-6217677',
+    'latitude': '25.273590',
+    'longitude': '119.196220',
+    'preview_image': 'http://img.leshare.shop/ptect/xy-1.jpg/small',
+    'images': [
+      'http://img.leshare.shop/ptect/xy-1.jpg',
+      'http://img.leshare.shop/ptect/xy-2.jpg'
+    ]
+  },
+  {
+    'seq': '6',
+    'name': '湄洲岛征管所网点',
+    'address': '莆田市秀屿区山亭乡利山村高速公路湄洲岛征管所18道',
+    'phone': '0594-2353223',
+    'latitude': '25.165200',
+    'longitude': '119.115600',
+    'preview_image': 'http://img.leshare.shop/ptect/mzd-1.jpg/small',
+    'images': [
+      'http://img.leshare.shop/ptect/mzd-1.jpg',
+      'http://img.leshare.shop/ptect/mzd-2.jpg'
+    ]
+  },
+  {
+    'seq': '7',
+    'name': '仙游盖尾所网点',
+    'address': '莆田市仙游县盖尾镇东井宫村仙游盖尾收费站10车道',
+    'phone': '0594-8053670',
+    'latitude': '25.383520',
+    'longitude': '118.791410',
+    'preview_image': 'http://img.leshare.shop/ptect/xygw-1.jpg/small',
+    'images': [
+      'http://img.leshare.shop/ptect/xygw-1.jpg',
+      'http://img.leshare.shop/ptect/xygw-2.jpg'
+    ]
+  },
+  {
+    'seq': '8',
+    'name': '仙游龙华所网点',
+    'address': '仙游县龙华镇貂峰村仙游龙华高速公路征管所14车道',
+    'phone': '0594-8179166',
+    'latitude': '25.342630',
+    'longitude': '118.630500',
+    'preview_image': 'http://img.leshare.shop/ptect/xylh-1.jpg/small',
+    'images': [
+      'http://img.leshare.shop/ptect/xylh-1.jpg',
+      'http://img.leshare.shop/ptect/xylh-2.jpg'
+    ]
+  }
+];
---
+++ src/utils/Constants.js
@@ -0,0 +1,39 @@
+export const GOODSTYPE = [{
+    pic: 'jiadian',
+    text: '家电',
+    val: '0'
+  }, {
+    pic: 'diannao',
+    text: '电脑',
+    val: '1'
+  }, {
+    pic: 'shouji',
+    text: '手机',
+    val: '2'
+  }, {
+    pic: 'boli',
+    text: '玻璃',
+    val: '3'
+  }, {
+    pic: 'jinshu',
+    text: '金属',
+    val: '4'
+  }, {
+    pic: 'zhiban',
+    text: '纸板',
+    val: '5'
+  }, {
+    pic: 'mucai',
+    text: '木材',
+    val: '6'
+  }, {
+    pic: 'yifu',
+    text: '衣服',
+    val: '7'
+  }, {
+    pic: 'more',
+    text: '更多',
+    val: '8'
+  }]
+
+export const ADDRESS_LIST_UPDATE = 'ADDRESS_LIST_UPDATE'
